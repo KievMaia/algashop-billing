797a4e34003558d756db3a2d774a36e9861d089a
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2026-03-23 12:52:01 -0300

    Implementa testes unitários para a Invoice.

--- src/test/java/com/algaworks/algashop/billing/domain/model/invoice/InvoiceTest.java
+++ src/test/java/com/algaworks/algashop/billing/domain/model/invoice/InvoiceTest.java
@@ -1,9 +1,11 @@
 package com.algaworks.algashop.billing.domain.model.invoice;
 
+import com.algaworks.algashop.billing.domain.model.DomainException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -11,29 +13,15 @@ class InvoiceTest {
 
     @Test
     public void shouldCreateInvoiceIssue() {
-        var address = Address.builder()
-                .street("rua a")
-                .state("california")
-                .number("123")
-                .city("Berlin")
-                .zipCode("12345")
-                .neighborhood("12314546")
-                .build();
-        var payer = Payer.builder()
-                .fullName("Kiev Maia")
-                .phone("123456789")
-                .email("kievmaia@gmail.com")
-                .document("123456")
-                .address(address)
-                .build();
-        var item = LineItem.builder()
-                .name("Kiev Maia")
-                .amount(BigDecimal.ONE)
-                .number(2)
-                .build();
-        var invoice = Invoice.issue("123", UUID.randomUUID(), payer, Set.of(item));
+        var invoice = InvoiceTestDataBuilder.anInvoice().build();
+        var orderId = invoice.getOrderId();
+        var customerId = invoice.getCustomerId();
+        var payer = invoice.getPayer();
+        var items = invoice.getItems();
 
-        Assertions.assertThat(invoice).isNotNull();
+        var invoiceIssue = Invoice.issue(orderId, customerId, payer, items);
+
+        Assertions.assertThat(invoiceIssue).isNotNull();
     }
 
     @Test
@@ -52,5 +40,81 @@ class InvoiceTest {
         Assertions.assertThat(invoice.getStatus()).isEqualTo(InvoiceStatus.CANCELED);
         Assertions.assertThat(invoice.getCancelledAt()).isNotNull();
         Assertions.assertThat(invoice.getCancelReason()).isEqualTo("Canceled");
+    }
+
+    @Test
+    public void shouldChangePaymentSettings() {
+        var creditCardId = UUID.randomUUID();
+        var invoice = InvoiceTestDataBuilder.anInvoice()
+                .paymentSettings(PaymentMethod.GATEWAY_BALANCE, creditCardId)
+                .build();
+
+        invoice.changePaymentSettings(PaymentMethod.CREDIT_CARD, UUID.randomUUID());
+
+        Assertions.assertThat(invoice.getPaymentSettings().getPaymentMethod()).isNotEqualTo(PaymentMethod.GATEWAY_BALANCE);
+        Assertions.assertThat(invoice.getPaymentSettings().getCreditCardId()).isNotEqualTo(creditCardId);
+    }
+
+    @Test
+    public void shouldAttributeGatewayCodeCorrectly() {
+        var invoice = InvoiceTestDataBuilder.anInvoice().paymentSettings(PaymentMethod.CREDIT_CARD, UUID.randomUUID()).build();
+        invoice.assignPaymentGatewayCode("123");
+
+        Assertions.assertThat(invoice.getPaymentSettings().getGatewayCode()).isNotNull();
+        Assertions.assertThat(invoice.getPaymentSettings().getGatewayCode()).isEqualTo("123");
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCreateInvoiceWithEmptyItems() {
+        Assertions.assertThatThrownBy(() -> InvoiceTestDataBuilder.anInvoice()
+                .items(new HashSet<>())
+                .build()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSetPaidInvoiceAlreadyCanceled() {
+        var invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.markAsCancelled("Canceled");
+
+        Assertions.assertThatThrownBy(invoice::markAsPaid).isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSetCanceledInvoiceAlreadyCanceled() {
+        var invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.markAsCancelled("Canceled");
+
+        Assertions.assertThatThrownBy(
+                () -> invoice.markAsCancelled("Cancelar")
+        ).isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTryChangePaymentSettingsOfAlreadyPaidInvoice() {
+        var invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.markAsPaid();
+
+        Assertions.assertThatThrownBy(
+                () -> invoice.changePaymentSettings(PaymentMethod.CREDIT_CARD, UUID.randomUUID())
+        ).isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTryChangeGatewayCodeOfAlreadyPaidInvoice() {
+        var invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.markAsPaid();
+
+        Assertions.assertThatThrownBy(
+                () -> invoice.assignPaymentGatewayCode("123")
+        ).isInstanceOf(DomainException.class);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTryChangItemsList() {
+        var invoice = InvoiceTestDataBuilder.anInvoice().build();
+
+        Assertions.assertThatThrownBy(
+                () -> invoice.getItems().clear()
+        ).isInstanceOf(UnsupportedOperationException.class);
     }
 }
